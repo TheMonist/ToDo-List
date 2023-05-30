3ce564330fb94dc0c614e5d7adacb15e6f5a177f
TheMonist <rogerrosena@gmail.com>
2023-05-29 21:38:13 -0400

    adding factory functions

---
+++ src/domHandler.js
@@ -0,0 +1,28 @@
+import {todoList} from "./src/todo.js";
+
+let modal = document.querySelector("#modal");
+let modalOpen = document.querySelector(".modalOpen");
+let modalClose = document.querySelector(".modalClose");
+let addToDo = document.querySelector(".addToDo");
+
+//create
+//read
+//update
+//delete
+
+//modals
+function openModal () {
+    modal.classList.delete("hidden");
+}
+function closeModal () {
+    modal.classList.add("hidden");
+}
+
+function appendToDo () {
+    addToDo.addEventListener("click", e => {
+        //fuction to add todo
+    });
+}
+
+modalOpen.addEventListener("click", openModal);
+modalClose.addEventListener("click", closeModal);
---
+++ src/todo.js
@@ -0,0 +1,10 @@
+const todoList = (title, description, dueDate, priority) => {
+    return  {
+        title,
+        description,
+        dueDate,
+        priority
+    }
+};
+
+export {todoList}
